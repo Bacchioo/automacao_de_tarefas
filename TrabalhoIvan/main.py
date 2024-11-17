4bbdf33b98f743c95afcfb4494e091fb199f8cec
Bacchioo <124638567+Bacchioo@users.noreply.github.com>
2024-11-17 15:27:13 -0300

    FOI PORRA

--- TrabalhoIvan/main.py
+++ TrabalhoIvan/main.py
@@ -4,56 +4,46 @@ from selenium.webdriver.common.by import By
 import pandas as pd
 import wget
 
-planilha = pd.DataFrame(columns=['Número', 'Nome', 'Altura', 'Imagem'])
+planilha = pd.DataFrame(columns=['Nome', 'Preco', 'Imagem'])
 
 #criar um objeto para manipular o navegador
 chrome = webdriver.Chrome()
 
 #abrir uma página no chrome
-chrome.get('https://sg.portal-pokemon.com/play/pokedex/0001')
+chrome.get('https://www.kabum.com.br/perifericos/-mouse-gamer/com-fio')
 
+#Procurar pelo elemento que possui o nome
+elementNome = chrome.find_elements(By.CLASS_NAME, "sc-d79c9c3f-0")
+
+#Procurar pelo elemento que possui o preco
+elementPreco = chrome.find_elements(By.CLASS_NAME, "sc-57f0fd6e-2")
+
+#procurar pela imagem
+elementImage = chrome.find_elements(By.CLASS_NAME, "imageCard")
 contador = 0
 
 while contador < 10:
-
-    #Procurar pelo elemento que possui o código do pokemon
-    elementNumero = chrome.find_element(By.CLASS_NAME, "pokemon-slider__main-no")
-
-    #Procurar pelo elemento que possui o nome do pokemon
-    elementNome = chrome.find_element(By.CLASS_NAME, "pokemon-slider__main-name")
-
-    #procura pela altura do pokemon
-    elementAltura = chrome.find_element(By.CSS_SELECTOR, ".pokemon-info__height .pokemon-info__value")
-
-    #procurar pela imagem
-    elementImage = chrome.find_element(By.CLASS_NAME, "pokemon-img__front")
-    enderecoImage = elementImage.get_attribute('src')
+    enderecoImage = elementImage[contador].get_attribute('src')
 
     #cria um novo nome para a imagem
-    urlNavegador = chrome.current_url
-    numeroPokemonVersao = urlNavegador.split("/")[-1]
+    nomeProduto = "RATOS" + str(contador)
     extensaoImg = enderecoImage.split(".")[-1]
-    nomeImagem = numeroPokemonVersao + "." + extensaoImg
+    nomeImagem = nomeProduto + "." + extensaoImg
     ondeSalvar = "TrabalhoIvan\\img\\" + nomeImagem
 
     wget.download(enderecoImage, ondeSalvar)
 
-    print("Numero", elementNumero.text)
-    print("Nome: ", elementNome.text)
-    print("Altura: ", elementAltura.text)
+    print("Nome: ", elementNome[contador].text)
+    print("Preço: ", elementPreco[contador].text)
 
     #adiciona os dados na planilha
-    planilha.loc[contador] = [elementNumero.text, elementNome.text, elementAltura.text, nomeImagem]
-
-    #seleciona o botão para o proximo pokemon
-    btnNext = chrome.find_element(By.CSS_SELECTOR, ".pokemon-slider__wrapper--right > a")
-    chrome.execute_script('arguments[0].click()', btnNext)
+    planilha.loc[contador] = [elementNome[contador].text, elementPreco[contador].text, nomeImagem]
 
     #incremento para a parada do while
     contador = contador + 1
 
 #salva a planilha em formato excel
-planilha.to_excel('pokemon.xlsx', index=False)
+planilha.to_excel('RATOS.xlsx', index = True)
 
 
 #mantém a página aberta
--- teste.py
+++
@@ -1,3 +0,0 @@
-import pyautogui
-
-pyautogui.click(300, 430, duration = 0.5, clicks = 2)
